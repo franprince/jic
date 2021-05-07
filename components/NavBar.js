79e107a09582808948e14d42762b881118118708
Julieta Mullen <mul.julieta@gmail.com>
2021-05-07 17:17:53 -0300

    commit

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,43 +3,10 @@ import styles from "../styles/NavBar.module.css"
 import {Spin as Hamburger} from 'hamburger-react'
 import { motion } from "framer-motion"
 
-export default function Navbar({breakpointsSmall, breakpointsBig, breakpointsMobile, size}) {
+export default function Navbar({color, scroll}) {
 
-    var breakpoints = []
-
-    if (size.height <= 650) {
-        var breakpoints = breakpointsMobile
-    } else if (size.height < 1100) {
-        var breakpoints = breakpointsSmall
-    } else {
-        var breakpoints = breakpointsBig
-    }
-
-    var combinedBreakpoints = []
-
-    const range = (start, end) => {
-        const length = end - start;
-        return Array.from({ length }, (_, i) => start + i);
-    }
-
-    for (let i = 0; i < breakpoints.length; i++) {
-        const br = range(breakpoints[i][0], breakpoints[i][1]);
-        combinedBreakpoints = [...combinedBreakpoints, br]
-    }
-
-    var merged = [].concat.apply([], combinedBreakpoints)
 
     const [open, setOpen] = useState(true)
-    const [scroll, setScroll] = useState(0); // Defino el estado de scroll de la página
-
-    // Hook para detectar el scroll de la página y cambiar la barra de navegación
-    useEffect(() => {
-        const scroll = () => {
-        setScroll(window.scrollY); // Cada vez que la página scrollea, el estado se actualiza al valor de scroll
-        };
-        window.addEventListener("scroll", scroll, false);
-        return () => window.removeEventListener("scroll", scroll, false);
-    }, []);
 
     const handleClick = () => { // Abre y cierra la barra de navegación haciéndole click
         setOpen(!open)
@@ -50,8 +17,6 @@ export default function Navbar({breakpointsSmall, breakpointsBig, breakpointsMob
         } else {
             setOpen(true)
         }
-
-        console.log(scroll)
     }, [scroll])
 
     // Variantes para las animaciones con Framer Motion.
@@ -95,7 +60,7 @@ export default function Navbar({breakpointsSmall, breakpointsBig, breakpointsMob
     return (
     <div className={styles.wrapper}>
         <main>
-        <h2 style={merged.includes(scroll) ? {color: "#222222"} : {color: "white"}}>JIC</h2>
+        <h2 style={{"color": color}}>JIC</h2>
         <motion.nav className={styles.nav} animate={open ? "open" : "closed"} variants={variants} transition={{ type: "spring", duration: 1, velocity: 2 }}>
         <motion.div className={styles.prueba} animate={open && scroll > 200 ? "open" : "closed"} variants={circleVariants} transition={{ type: "ease", duration: 0.1}}>
         </motion.div>
@@ -108,7 +73,7 @@ export default function Navbar({breakpointsSmall, breakpointsBig, breakpointsMob
                 <li style={scroll < 200 ? liLast : liScrolled}>CONTACTO</li>
             </ul>
         </motion.nav>
-        <motion.div className={styles.hamb} style={merged.includes(scroll) ? {color: "#222222"} : {color: "white"}} animate={scroll < 200 ? {opacity: 0, display: "none"} : {opacity: 1}} transition={{duration: 0.5}} >
+        <motion.div className={styles.hamb} animate={scroll < 200 ? {opacity: 0, display: "none"} : {opacity: 1}} transition={{duration: 0.5}} >
         <Hamburger toggled={open} toggle={handleClick} size={18} className={styles.hamb}/>
         </motion.div>
         </main>
--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,34 @@ import ContactCard from "../components/ContactCard"
 import WorkTogether from "../components/WorkTogether"
 import Footer from "../components/Footer"
 
+import { InView, useInView } from 'react-intersection-observer';
+
 import {useState, useEffect} from "react"
 
 import miscPictures from "../miscPictures.json" /* Imágenes para header y separadores */
 
 export default function Home() {
 
+
+  const [color, setColor] = useState("#FFF")
+  const [scroll, setScroll] = useState(0); // Defino el estado de scroll de la página
+
   const headerImg = miscPictures[0].img
   const separator1 = miscPictures[1].img
   const separator1mobile = miscPictures[1].mobileImg
   const contactPic = miscPictures[2].img
 
-  const breakpointsMobile = [[2337, 2916], [3724, 4000]]
-  const breakpointsSmall = [[2156, 2796], [3603, 4003]]
-  const breakpointsBig = [[3085, 4404], [5838, 6000]]
-  
   const size = useWindowSize();
+
+
+  // Hook para detectar el scroll de la página y cambiar la barra de navegación
+  useEffect(() => {
+      const scroll = () => {
+      setScroll(window.scrollY); // Cada vez que la página scrollea, el estado se actualiza al valor de scroll
+      };
+      window.addEventListener("scroll", scroll, false);
+      return () => window.removeEventListener("scroll", scroll, false);
+  }, []);
 
   // Hook para detectar el tamaño de pantalla.
 function useWindowSize() {
@@ -55,17 +67,23 @@ if (typeof window !== 'undefined') {
 return windowSize;
 }
 
+
+
   return (<>
       <Head>
         <title>Next App</title>
       </Head>
-      <NavBar breakpointsBig={breakpointsBig} breakpointsSmall={breakpointsSmall} breakpointsMobile={breakpointsMobile} size={size}/>
+      <NavBar size={size} color={color} scroll={scroll}/>
       <Header img={headerImg} title="JUAN IGNACIO CALI" subtitle="Filmmaker | Director Creativo | Motion Designer"/>
       <Featured/>
       <Separator img={separator1} mobileImg={separator1mobile} size={size}/>
+      <InView threshold="0.5" onChange={(inView) => inView ? setColor("#222") : setColor("#FFF")}>
       <Services/>
+      </InView>
       <ContactCard img={contactPic}/>
+      <InView threshold="0.5" onChange={(inView) => inView ? setColor("#222") : setColor("#FFF")}>
       <WorkTogether/>
+      </InView>
       <Footer/>
       </>
   )
