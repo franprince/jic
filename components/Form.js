3fe2fae849ab3bf0353420a2e038c33bee4d87b5
Julieta Mullen <mul.julieta@gmail.com>
2021-07-21 03:33:40 -0300

    Delete log

--- components/Form.js
+++ components/Form.js
@@ -146,7 +146,6 @@ export default function Form ({sent, setSent, size}) {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ nombre: nombre, mensaje: mensaje, presupuesto: presupuesto, producto: producto, trabajo: trabajo, puesto: puesto, email: email })
         });
-        console.log("sent")
         setSent(true)
         formName.current.value = ""
         formEmail.current.value = ""
