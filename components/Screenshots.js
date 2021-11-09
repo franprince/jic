c433da64644d3a490c05f70318256f40f6e495c5
Julieta Mullen <mul.julieta@gmail.com>
2021-11-09 16:33:53 -0300

    Fix bug in screenshots

--- components/Screenshots.js
+++ components/Screenshots.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from "react"
-import styles from "../styles/Screenshots.module.css"
+import styles from "../styles/Screenshots.module.css";
 
-export default function Screenshots ({pictures, last}) {
-
-    return (
-        <section className={pictures.length > 1 ? styles.container : styles.single}>
-            {pictures.length > 1 ? pictures.map((item, index) => {
-                return (
-                    <article key={item}>
-                        <img src={item} alt="Screenshot" />
-                    </article>
-                )
-            }) :
-            <article key={pictures} style={{gridColumn: "1/-1"}}>
-                <img src={pictures} alt="Screenshot" />
-            </article>}
-
+export default function Screenshots({ pictures }) {
+  return (
+    <>
+      {pictures.length !== 0 ? (
+        <section
+          className={pictures.length > 1 ? styles.container : styles.single}
+        >
+          {pictures.length > 1 ? (
+            pictures.map((item, index) => {
+              return (
+                <article key={item}>
+                  <img src={item} alt="Screenshot" />
+                </article>
+              );
+            })
+          ) : (
+            <article key={pictures} style={{ gridColumn: "1/-1" }}>
+              <img src={pictures[0]} alt="Screenshot" />
+            </article>
+          )}
         </section>
-    )
+      ) : null}
+    </>
+  );
 }
