bb2f7f13ad93d819f5913d64592ffe744db60f06
Fran <fran.p.21@gmail.com>
2022-08-02 19:01:23 -0300

    Add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,121 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <meta charSet="utf-8" />
+          <meta
+            content="Director Creativo & Filmmaker. Me gusta contar historias y viajar."
+            name="description"
+          />
+          <meta
+            content="Director Creativo & Filmmaker. Me gusta contar historias y viajar."
+            name="keywords"
+          />
+          <meta
+            content="https://cdn.sanity.io/images/arac2r4p/production/ac7d7bf833f97cfbed82863e0026feedc2e564b8-1080x1080.jpg"
+            name="image"
+          />
+          <meta content="https://juanignaciocali.com" property="og:url" />
+          <meta content="Juan Ignacio Cali" property="og:title" />
+          <meta
+            content="Director Creativo & Filmmaker. Me gusta contar historias y viajar."
+            property="og:description"
+          />
+          <meta
+            content="https://cdn.sanity.io/images/arac2r4p/production/ac7d7bf833f97cfbed82863e0026feedc2e564b8-1080x1080.jpg"
+            property="og:image"
+          />
+          <link rel="canonical" href="https://juanignaciocali.com" />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/BarlowCondensed-ExtraLightItalic.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/BarlowCondensed-LightItalic.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Gobold Bold Italic.otf"
+            type="font/opentype"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Lora-Italic-VariableFont_wght.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Lora-VariableFont_wght.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-Bold.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-ExtraBold.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-ExtraLight.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-Light.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-LightItalic.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-Regular.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-SemiBold.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/Poppins-ThinItalic.ttf"
+            type="application/octet-stream"
+            crossorigin="anonymous"
+          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
