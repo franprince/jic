6bc37a1343e9a34b6d261fe9d6dfa21ded0c0220
Fran <fran.p.21@gmail.com>
2023-10-23 13:17:22 -0300

    Secure links

--- components/Description.js
+++ components/Description.js
@@ -2,6 +2,14 @@ import DOMPurify from "isomorphic-dompurify";
 import { marked } from "marked";
 import styles from "../styles/Description.module.css";
 
+DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+  // set all elements owning target to target=_blank
+  if ('target' in node) {
+    node.setAttribute('target', '_blank');
+    node.setAttribute('rel', 'noopener');
+  }
+});
+
 export default function Description({ title, text, credits }) {
   const markup = {__html: DOMPurify.sanitize(marked(text))}
   return (
